e461aa50c7ea28419c329fb97362fb1b0c51c550
adogbeji <baka_1993@yahoo.co.uk>
2024-11-22 19:46:36 +0000

    Start building oinboarding screen

--- lib/views/screens/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen.dart
@@ -5,9 +5,17 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Onboarding Screen'),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/onboarding.png'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
