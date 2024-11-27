312348f9a5e1fecb0ef34300baeab629c5414049
adogbeji <baka_1993@yahoo.co.uk>
2024-11-27 20:18:23 +0000

    Comment out image & add more text in onboarding screen

--- lib/views/screens/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen.dart
@@ -8,24 +8,27 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          // Container(
+          //   decoration: const BoxDecoration(
+          //     image: DecorationImage(
+          //       image: AssetImage('assets/images/onboarding.png'),
+          //       fit: BoxFit.cover,
+          //     ),
+          //   ),
+          // ),
           Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/onboarding.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            child: Column(
+            child: const Column(
               children: [
                 Text(
                   'Premium cars. Enjoy the luxury',
                   style: TextStyle(
                     fontSize: 32,
+                    color: Colors.black54,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+                SizedBox(height: 10,),
+                Text('Premium and prestigious daily car rental.\nExperience the thrill at a lower price')
               ],
             ),
           ),
