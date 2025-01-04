c66d554ad06c912a605095978fae676d8b9a8c3e
adogbeji <baka_1993@yahoo.co.uk>
2025-01-04 09:54:04 +0000

    Change background colour of onboarding screen

--- lib/views/screens/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen.dart
@@ -6,9 +6,11 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey.shade900,
       body: Column(
         children: [
           Expanded(
+            flex: 2,
             child: Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
@@ -18,49 +20,53 @@ class OnboardingScreen extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            child: Column(
-              children: [
-                const Text(
-                  'Premium cars. Enjoy the luxury',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.black54,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                const SizedBox(height: 10,),
-
-                const Text(
-                  'Premium and prestigious daily car rental.\nExperience the thrill at a lower price',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-
-                const SizedBox(height: 20,),
-
-                SizedBox(
-                  width: 320,
-                  height: 54,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Premium cars. Enjoy the luxury',
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
-                    child: const Text(
-                      'Let\'s Go',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                  ),
+            
+                  const SizedBox(height: 10,),
+            
+                  const Text(
+                    'Premium and prestigious daily car rental.\nExperience the thrill at a lower price',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+            
+                  const SizedBox(height: 20,),
+            
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Text(
+                        'Let\'s Go',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
