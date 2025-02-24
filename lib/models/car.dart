003b3460c90a203b4197d40d4ac4fc2b94527df8
adogbeji <baka_1993@yahoo.co.uk>
2025-02-24 14:57:45 +0000

    Add constructor to Car class

--- lib/models/car.dart
+++ lib/models/car.dart
@@ -3,4 +3,6 @@ class Car {
   final double distance;
   final double fuelCapacity;
   final double pricePerHour;
+
+  Car({required this.model, required this.distance, required this.fuelCapacity, required this.pricePerHour});
 }
