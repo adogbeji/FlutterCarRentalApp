ac399a83c2e85e41bdc70e94660835d8562842ff
adogbeji <baka_1993@yahoo.co.uk>
2025-02-27 14:45:35 +0000

    Add starting code for CarCard widget

---
+++ lib/views/widgets/car_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CarCard extends StatelessWidget {
+  const CarCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Card Card'),
+      ),
+    );
+  }
+}
