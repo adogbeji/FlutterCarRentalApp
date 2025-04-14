32eef049ff27bd561d90d5fa9bf81a747e6a6e4c
adogbeji <baka_1993@yahoo.co.uk>
2025-04-14 14:50:52 +0100

    Start displaying image in CarCard

--- lib/views/widgets/car_card.dart
+++ lib/views/widgets/car_card.dart
@@ -1,12 +1,23 @@
+// import 'package:car_rental_app/models/car.dart';
 import 'package:flutter/material.dart';
+import 'package:car_rental_app/models/car.dart';
 
 class CarCard extends StatelessWidget {
-  const CarCard({super.key});
+  final Car car;
+
+  // const CarCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Image.asset('assets/images//car_image.png', height: 120,),
+          Text(car.model),
+        ]
+      ),
     );
   }
 }
